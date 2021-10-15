d1c87b934f8ede4af4f5c2033af54c3ba93c46c1
Serg Tyatin <700@2rba.com>
2021-10-15 21:30:09 +0100

    ide with pipe_id param

---
+++ ide/precache-manifest.19807c4d9616aad4f8d11c8608a656db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a10fc3f7d32a8cac156480211c5946e",
+    "url": "/ide/index.html"
+  },
+  {
+    "revision": "ad6aa3478f73330c9428",
+    "url": "/ide/static/css/main.e8f754fe.chunk.css"
+  },
+  {
+    "revision": "58b38ba2b3811964cdac",
+    "url": "/ide/static/js/2.a84d1281.chunk.js"
+  },
+  {
+    "revision": "5fe7d013febeee9247229a7cf8ff3682",
+    "url": "/ide/static/js/2.a84d1281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6aa3478f73330c9428",
+    "url": "/ide/static/js/main.1abce91a.chunk.js"
+  },
+  {
+    "revision": "e3f2a811247c0fe7c22d",
+    "url": "/ide/static/js/runtime-main.6d878f3a.js"
+  }
+]);
